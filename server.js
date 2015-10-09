6ccbf31b0104fe796624e0936c1d6f09978752a8
Yanelen <yanelen@Yanelen.local>
2015-10-09 11:05:22 -0400

    class

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 var express = require('express'),
     PORT = process.env.PORT || 5432,
-    server = express();
+    server = express(),
+    MONGOURI = process.ev.MONGOLAB_URL || "mongodb://localhost:2701",
+    dbname = "some_useful _name",
+    mongoose = require('mongoose');
 
 server.get('/test', function(req, res) {
   res.write("Welcome to my amazing app");
   res.end();
 })
+
+mongoose.connect(MONGOURI + "/" + dbname);
 
 server.listen(PORT, function() {
   console.log("SERVER IS UP ON PORT:", PORT)
